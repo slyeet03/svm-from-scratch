a51cb3a7402888a4609bcec2991c91bced82ce3d
slyeet03 <shobhit7419@gmail.com>
2026-05-10 00:04:58 +0530

    implemented rbf kernel

--- src/kernel.rs
+++ src/kernel.rs
@@ -16,3 +16,22 @@ impl Kernel for Linear {
         sum
     }
 }
+
+pub struct RBF {
+    pub sigma: f64,
+}
+
+impl Kernel for RBF {
+    fn compute(&self, xi: &[f64], xj: &[f64]) -> f64 {
+        let k: usize = xi.len();
+        let mut euc_dist: f64 = 0.0;
+        let mut exp: f64 = 0.0;
+
+        for n in 0..k {
+            euc_dist += (xi[n] - xj[n]).powf(2.0);
+        }
+
+        exp = (-euc_dist) / (2.0 * self.sigma);
+        exp.exp()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::kernel::Kernel;
 mod kernel;
 
 fn main() {
-    let ker = kernel::Linear;
+    let ker = kernel::RBF { sigma: 1.0 };
 
     let sum: f64 = ker.compute(&[2.0_f64, 3.0_f64], &[4.0_f64, 1.0_f64]);
 
